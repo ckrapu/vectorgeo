a5a97ed776c4d1d21e369a5365444e64b1972bcf
Chris Krapu <ckrapu@gmail.com>
2023-08-31 23:30:31 -0500

    Extending H3 state tracker with save to disk

--- vectorgeo/h3_utils.py
+++ vectorgeo/h3_utils.py
@@ -1,6 +1,47 @@
 import h3
-
+import json
 from collections import deque
+
+class H3GlobalIterator:
+    """
+    Represents the linear iteration over all H3 cells at a given resolution, starting
+    at a seed resolution. Provides the ability to save and restore state.
+    """
+    def __init__(self, seed_lat, seed_lng, resolution, state_file='h3_state.json'):
+        self.resolution = resolution
+        self.state_file = state_file
+        try:
+            with open(self.state_file, 'r') as f:
+                state = json.load(f)
+                self.seen = set(state['seen'])
+                self.queue = deque(state['queue'])
+        except FileNotFoundError:
+            print(f"State file {self.state_file} not found; starting from seed")
+            self.seen = set()
+            self.queue = deque()
+            seed = h3.geo_to_h3(seed_lat, seed_lng, resolution)
+            self.queue.append(seed)
+            self.seen.add(seed)
+    
+    def save_state(self):
+        with open(self.state_file, 'w') as f:
+            state = {'seen': list(self.seen), 'queue': list(self.queue)}
+            json.dump(state, f)
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if not self.queue:
+            raise StopIteration
+        current_cell = self.queue.popleft()
+        neighbors = h3.k_ring(current_cell, 1)
+        for neighbor in neighbors:
+            if neighbor not in self.seen:
+                self.seen.add(neighbor)
+                self.queue.append(neighbor)
+        return current_cell
+
 
 def h3_global_iterator(seed_lat, seed_lng, resolution):
     """
