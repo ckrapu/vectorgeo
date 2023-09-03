bdca3624281ad11dac118e6f0ded56baa7de3760
Chris Krapu <ckrapu@gmail.com>
2023-09-03 17:41:20 -0400

    Utility & flow for identifying valid H3 cells for inference

---
+++ 00-h3-stencil-flow.py
@@ -0,0 +1,104 @@
+import geopandas as gpd
+import rasterio as rio
+import h3
+import numpy as np
+
+from metaflow import FlowSpec, step, Parameter, parallel_map
+from rasterio import features
+from tqdm import tqdm
+
+from vectorgeo.h3_utils import generate_h3_indexes_at_resolution
+
+class H3StencilFlow(FlowSpec):
+    """
+    Creates a binary raster mask of the world, where 1s represent land and 0s represent water.
+    This mask is then used to stencil out H3 cells, avoiding those which are entirely water.
+    """
+
+    n_cells = Parameter('n_cells',
+                        help="Number of cells in both x and y direction for binary mask",
+                        default=1000)
+
+    h3_resolution = Parameter('h3_resolution',
+                              help="H3 resolution level",
+                              default=7)
+
+    n_batches = Parameter('n_batches',
+                            help="Number of batches to split the H3 cells into",
+                            default=8)
+    
+    @step
+    def start(self):
+        """
+        Prepares set of all H3 cell IDs as well as a buffered and simplified
+        world geometry for masking out in the following step.
+        """
+
+        # Generate all h3 cells at a given resolution by considering
+        # all hexadecimal numbers of the appropriate length with
+        # f values at the end
+        self.h3s = generate_h3_indexes_at_resolution(self.h3_resolution)
+        print(f"After generating all h3s, there are {len(self.h3s)} cells")
+
+        crs = 'EPSG:4326'
+
+        print("Reading world geometry")
+        world_gdf = gpd.read_file('tmp/world.gpkg')
+        world_gdf.geometry = world_gdf.buffer(0.05).simplify(0.1)
+        self.world_gdf = world_gdf.to_crs(crs)
+
+        self.next(self.end)
+
+
+    @step
+    def end(self):
+        # Here, we simplify + buffer the geometry and then
+        # convert it into a binary raster mask
+        x_res = self.n_cells
+        y_res = self.n_cells
+
+        # Create the raster
+        print("Creating raster")
+        
+        # Bounds should run over all of earth
+        bounds = (-180, -90, 180, 90)
+
+        transform = rio.transform.from_bounds(*bounds, x_res, y_res)
+
+        image = features.rasterize(
+                    ((geom, 1) for geom in self.world_gdf.geometry),
+                    out_shape=(y_res, x_res),
+                    transform=transform
+        )
+
+        # For all H3 cells, check whether their centroid lands on a 1 cell;
+        # if they do, add them to the set. Otherwise, discard them.
+        print("Processing H3 cells")
+        h3_batches = [x.tolist() for x in np.array_split(list(self.h3s), self.n_batches)]
+
+        def stencil_h3_batch(h3s):
+            """
+            Filters out any H3 cells which aren't on or close to land.
+            """
+            h3s_processed = set()
+
+            for cell in tqdm(h3s):           
+
+                centroid = h3.h3_to_geo(cell)
+                lat, lng = centroid
+                row, col = rio.transform.rowcol(transform, lng, lat)
+                val = image[row, col]
+
+                if val == 1:
+                    h3s_processed.add(cell)
+
+            return h3s_processed
+
+        self.h3s_processed = parallel_map(stencil_h3_batch, h3_batches)
+        self.h3s_processed = list(set.union(*self.h3s_processed))
+
+        # print fraction of cells which are added to set
+        print(f"Retained {len(self.h3s_processed) / len(self.h3s)} of cells")
+    
+if __name__ == '__main__':
+    H3StencilFlow()
--- vectorgeo/h3_utils.py
+++ vectorgeo/h3_utils.py
@@ -44,33 +44,24 @@ class H3GlobalIterator:
         return current_cell
 
 
-def h3_global_iterator(seed_lat, seed_lng, resolution):
-    """
-    A generator to iterate over all h3 cells globally at a given resolution.
-
-    Args:
-    - resolution (int): The resolution of the H3 index.
-
-    Yields:
-    - str: An H3 index.
-    """
+def generate_h3_indexes_at_resolution(resolution):
+    all_h3_indexes = set()
     
-    seen = set()  # Track cells we've already seen.
-    queue = deque()  # For BFS traversal.
+    # Start with the 122 base cells
+    base_cells = h3.get_res0_indexes()
     
-    # Seed cell at coordinates (0,0).
-    seed = h3.geo_to_h3(seed_lat, seed_lng, resolution)
-    queue.append(seed)
-    seen.add(seed)
+    # Recursive function to refine cells
+    def refine_cell(cell, current_res):
+        nonlocal all_h3_indexes
+        if current_res == resolution:
+            all_h3_indexes.add(cell)
+            return
+        children = h3.uncompact([cell], current_res + 1)
+        for child in children:
+            refine_cell(child, current_res + 1)
     
-    while queue:
-        current_cell = queue.popleft()
-        yield current_cell
-        
-        # Get the neighbors of the current cell.
-        neighbors = h3.k_ring(current_cell, 1)
-        
-        for neighbor in neighbors:
-            if neighbor not in seen:
-                seen.add(neighbor)
-                queue.append(neighbor)
+    # Start the recursion
+    for base_cell in base_cells:
+        refine_cell(base_cell, 0)
+    
+    return all_h3_indexes
