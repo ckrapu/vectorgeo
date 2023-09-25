5f78977dbf7f0f698258996775c5d407e839cb13
Chris Krapu <ckrapu@gmail.com>
2023-09-24 22:19:25 -0400

    Refactor dem generation to avoid Nones

--- 01-preprocess-flow.py
+++ 01-preprocess-flow.py
@@ -82,92 +82,76 @@ class PreprocessLandCoverFlow(FlowSpec):
         land cover patches and store them as Numpy arrays on S3.
         """
         self.uploaded_filekeys = []
-
-        print(f"Creating patch generator...")
+        
+        # Initialize patch generators
+        print("Creating patch generator...")
         lulc_generator = lc.RasterPatches(
             c.LC_LOCAL_PATH, self.world_gdf, self.patch_size, full_load=self.full_load
         )
-
         dem_generator = lc.RasterPatches(
             c.DEM_LOCAL_PATH, self.world_gdf, self.patch_size, full_load=self.full_load
         )
 
-        print(
-            f"Generating {self.n_files} files with {self.samples_per_file} samples each..."
-        )
-        for _ in range(self.n_files):
-            print(f"...Generating file {_ + 1} of {self.n_files}...")
-            file_id = abs(hash(str(time.time()))) % (10**6)
-            all_patches, all_patch_nbrs = [], []
+        # Generate files with samples
+        print(f"Generating {self.n_files} files with {self.samples_per_file} samples each...")
+        for file_num in range(self.n_files):
+            print(f"...Generating file {file_num + 1} of {self.n_files}...")
+            
+            # Initialize variables
+            file_id = abs(hash(str(time.time()))) % (10 ** 6)
+            lc_patches, lc_patch_nbrs = [], []
             all_pts, all_pt_nbrs = [], []
+            dem_patches, dem_patches_nbr = [], []
 
+            # Generate patches
             for (pt, patch), (pt_nbr, patch_nbr) in lulc_generator.generate_patches(
                 self.samples_per_file, create_pairs=True, pair_distance_meters=8_000
             ):
-                all_patches.append(patch)
-                all_patch_nbrs.append(patch_nbr)
-                all_pts.append(pt)
-                all_pt_nbrs.append(pt_nbr)
-
-            out_shape = len(all_patches), 1, self.patch_size, self.patch_size
-
-            all_patches = np.array(all_patches).reshape(*out_shape)
-            all_patch_nbrs = np.array(all_patch_nbrs).reshape(*out_shape)
-
-            patches_array = np.stack([all_patches, all_patch_nbrs], axis=-1)
-
-            # For Copernicus LC data, 255 is a "no data" value
-            has_nones = np.any(
-                np.isnan(patches_array) | patches_array == 255, axis=(1, 2, 3, 4)
-            )
-
-            if np.sum(has_nones) > 0:
-                print(
-                    f"Found {np.sum(has_nones)} patches with NaNs or 255s; removing them..."
-                )
-
-            # At this point, the shape is (N, 1, ny, nx, 2) where the 2 is for
-            # the anchor-neighbor pair.
-            patches_array = patches_array[~has_nones]
-            patches_array = np.vectorize(self.int_map.get)(patches_array)
-
-            # Using the point lists, use the DEM generator's extract_patch
-            # method to get the DEM images for each point, check them for NaNs
-            # and then concat them with the land cover data.
-            dem_patches = []
-            dem_patches_nbr = []
-
-            for pt, pt_nbr in zip(all_pts, all_pt_nbrs):
                 dem_patch = dem_generator.extract_patch(pt)
                 dem_patch_nbr = dem_generator.extract_patch(pt_nbr)
 
-                if dem_patch is None or dem_patch_nbr is None:
-                    raise ValueError("DEM patch is None")
+                # Skip if any patch is None
+                if any(x is None for x in [dem_patch, dem_patch_nbr, patch, patch_nbr]):
+                    continue
 
-                if dem_patch is not None and dem_patch_nbr is not None:
-                    dem_patches.append(dem_patch)
-                    dem_patches_nbr.append(dem_patch_nbr)
+                lc_patches.append(patch)
+                lc_patch_nbrs.append(patch_nbr)
+                all_pts.append(pt)
+                all_pt_nbrs.append(pt_nbr)
+                dem_patches.append(dem_patch)
+                dem_patches_nbr.append(dem_patch_nbr)
 
+            # Reshape and stack arrays
+            out_shape = len(lc_patches), 1, self.patch_size, self.patch_size
+            lc_patches = np.array(lc_patches).reshape(*out_shape)
+            lc_patch_nbrs = np.array(lc_patch_nbrs).reshape(*out_shape)
+            patches_array = np.stack([lc_patches, lc_patch_nbrs], axis=-1)
+
+            # Remove patches with NaNs or 255s
+            has_nones = np.any(np.isnan(patches_array) | (patches_array == 255), axis=(1, 2, 3, 4))
+            if np.sum(has_nones) > 0:
+                print(f"Found {np.sum(has_nones)} patches with NaNs or 255s; removing them...")
+            patches_array = patches_array[~has_nones]
+            patches_array = np.vectorize(self.int_map.get)(patches_array)
+
+            # Reshape and stack DEM arrays
             dem_patches = np.array(dem_patches).reshape(*out_shape)
             dem_patches_nbr = np.array(dem_patches_nbr).reshape(*out_shape)
+            dem_patches_array = np.stack([dem_patches, dem_patches_nbr], axis=-1)
 
-            dem_patches_array  = np.stack([dem_patches, dem_patches_nbr], axis=-1)
-
-            # Here, shape should be (N, 2, ny, nx, 2) where the second axis
-            # runs over [landcover, DEM] and the last axis runs over [anchor, neighbor]
+            # Concatenate landcover and DEM arrays
             patches_array = np.concatenate([patches_array, dem_patches_array], axis=1)
 
-            filename = (
-                f"lc-dem-patches-pairs-{self.patch_size}x{self.patch_size}-{file_id}.npy"
-            )
+            # Save and upload the file
+            filename = f"lc-dem-patches-pairs-{self.patch_size}x{self.patch_size}-{file_id}.npy"
             filepath = os.path.join(c.TMP_DIR, filename)
-
             np.save(filepath, patches_array)
             key = f"train/{filename}"
             transfer.upload_file(key, filepath)
             self.uploaded_filekeys.append(filename)
 
         self.next(self.join)
+
 
     @step
     def join(self, inputs):
