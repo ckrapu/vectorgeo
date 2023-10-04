1ac102e58b4dcf3c02e48476520a8d2fe68249d7
Chris Krapu <ckrapu@gmail.com>
2023-10-04 17:53:21 +0000

    Adding SENet layer

--- vectorgeo/models.py
+++ vectorgeo/models.py
@@ -4,6 +4,23 @@ import torch.nn.functional as F
 
 import numpy as np
 
+class SEBlock(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
 
 class ResBlockConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size):
@@ -20,6 +37,10 @@ class ResBlockConv(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
         out += identity
+
+        # Add SE block
+        out = SEBlock(out.shape[1])(out)
+
         return out
 
 
@@ -38,6 +59,9 @@ class ResBlockDense(nn.Module):
         out = self.fc2(out)
         out = self.bn2(out)
         out += identity
+
+        out = SEBlock(out.shape[1])(out)
+        
         return F.relu(out)
 
 
